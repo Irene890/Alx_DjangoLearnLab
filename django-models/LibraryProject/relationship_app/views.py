5f823b8d5a53243102b9487a30975dde2b14aeaa
Irene890 <munirene20@gmail.com>
2025-11-08 18:33:30 +0300

    User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,3 +13,41 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm
+
+# User registration view
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # after successful registration
+    else:
+        form = RegisterForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# User login view
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')  # redirect to home or any protected page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+
+# User logout view
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
