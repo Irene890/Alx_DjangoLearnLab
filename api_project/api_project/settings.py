03112eec94793a782c32de53164655621d767fb1
Irene890 <munirene20@gmail.com>
2025-11-22 17:23:39 +0300

    DRF

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@f25vv_e8t+lj(7!5^ta&wsdx*pp8-h=k(kh4#!7xckx@=!te#'
+SECRET_KEY = 'django-insecure-v$dt6rkb_^o8&o%gy%$5f%3l(g5e@b^j5+@o177#ae4ml%-kqr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    # Basic Django Admin
     path('admin/', admin.site.urls),
-    
-    # We will include the 'api' app's URLs here in future tasks
-    # path('api/v1/', include('api.urls')), 
 ]
