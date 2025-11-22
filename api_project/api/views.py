b055d68f3e4b1f1506692778f878047a3be99d0a
Irene890 <munirene20@gmail.com>
2025-11-22 18:01:21 +0300

    DRF2

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import viewsets
 
+# -----------------------------
+# 1. ListAPIView (previous task)
+# -----------------------------
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-
+# -----------------------------
+# 2. ViewSet for CRUD operations
+# -----------------------------
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
