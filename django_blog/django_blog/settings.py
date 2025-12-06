9709959e9b6cfe7d9131ea7bd76784393d452382
Irene890 <munirene20@gmail.com>
2025-12-06 09:09:21 +0300

    Django blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_blog_db',
+        'USER': 'postgres_user', # <-- USER setting
+        'PASSWORD': 'strongpassword',
+        'HOST': 'localhost',
+        'PORT': '5432', # <-- PORT setting
     }
 }
 
